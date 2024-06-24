1a7777a91d1d6293e53df4de486c3e1b32e3805e
ankit kumar <ant880423@gmail.com>
2024-06-24 15:31:04 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    // root route http://localhost:5000/
+    res.send("Ankit Kumar");
+});
+app.listen(5000, () => console.log('Server Running on port 5000'));
