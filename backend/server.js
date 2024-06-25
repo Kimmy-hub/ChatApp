65b4a05f5c36d7b260f3e849fe2f4d390faf3149
ankit kumar <ant880423@gmail.com>
2024-06-25 15:51:19 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
-const express = require('express');
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     // root route http://localhost:5000/
     res.send("Ankit Kumar");
 });
-app.listen(5000, () => console.log('Server Running on port 5000'));
+
+app.use("/api/auth",authRoutes);
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
