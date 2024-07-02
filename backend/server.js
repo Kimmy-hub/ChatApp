feb16ded5cc8d8cd3dc27815a64ccab21146eb8f
ankit kumar <ant880423@gmail.com>
2024-07-02 19:17:10 +0530

    SIGNUP

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongodb.js";
 
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Ankit Kumar");
-});
-
+dotenv.config();
+app.use(express.json());  // to parse the incoming requests with JSON payloads (from req.body)
 app.use("/api/auth", authRoutes);
-app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+
+
+// app.get("/", (req, res) => {
+//     // root route http://localhost:5000/
+//     res.send("Ankit Kumar");
+// });
+
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`)
+});
