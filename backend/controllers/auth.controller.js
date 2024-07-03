4f97ad0adf11db248fa9a56d9ba03a9fdbe804a7
ankit kumar <ant880423@gmail.com>
2024-07-03 09:51:53 +0530

    logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -19,7 +19,7 @@ export const signup = async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        
+
         //https://avatar-placeholder.iran.liara.run/
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
@@ -55,13 +55,40 @@ export const signup = async (req, res) => {
 };
 
 
-export const login = (req, res) => {
-    console.log("loginUser");
+export const login = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password,  user?.password || "");
+        if(!user || !isPasswordCorrect){
+            return res.status(401).json({error:"Invalid username or password"});
+        } 
+
+        generateTokenAndSetCookie(user._id, res);
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+            
+
+    } catch (error) {
+        console.log("Error in LogIn controller", error.message);
+        res.status(500).json({error:"internal server error"});
+    }
 };
 
 
-export const logout = (req, res) => {
-    console.log("logoutUser");
+export const logout = async (req, res) => {
+    try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out succesfully"});
+        
+    } catch (error) {
+        console.log("Error in LogOut controller", error.message);
+        res.status(500).json({error:"internal server error"});
+    }
 };
 
 
